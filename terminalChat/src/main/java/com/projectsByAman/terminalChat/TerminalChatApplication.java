994d4ef5ef8509f433d52cfe5d599456103312a2
cd-amanRepos <amanthapliyal3@gmail.com>
2021-05-05 01:16:34 +0530

    Project Structure

---
+++ terminalChat/src/main/java/com/projectsByAman/terminalChat/TerminalChatApplication.java
@@ -0,0 +1,13 @@
+package com.projectsByAman.terminalChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminalChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminalChatApplication.class, args);
+	}
+
+}
---
+++ terminalChat/src/test/java/com/projectsByAman/terminalChat/TerminalChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsByAman.terminalChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerminalChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
